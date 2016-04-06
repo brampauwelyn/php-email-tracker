c8ec50460fb57f0d419fb912954b1afeea64af32
brampauwelyn <brampauw@gmail.com>
2016-04-06 22:43:27 +0200

    Added test-email

--- tracker.php
+++ tracker.php
@@ -1,8 +1,11 @@
 <?php
 
+//THIS RETURNS THE IMAGE
 header('Content-Type: image/gif');
 readfile('tracking.gif');
 
+
+//THIS IS THE SCRIPT FOR THE ACTUAL TRACKING
 $date = date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']);
 $txt = $date.",". $_SERVER['REMOTE_ADDR'];
 $myfile = file_put_contents('log.txt', $txt.PHP_EOL , FILE_APPEND);
